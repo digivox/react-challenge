eba9238fd27da43b90435b736e53d006cffef92e
Jose Carlos <jcps.filho@gmail.com>
2020-08-03 21:07:34 -0300

    Remocao do domino biblioteca + finalizacao do rest

--- src/main/java/br/com/digivox/reactchallenge/repositories/AluguelRepository.java
+++ src/main/java/br/com/digivox/reactchallenge/repositories/AluguelRepository.java
@@ -1,5 +1,9 @@
 package br.com.digivox.reactchallenge.repositories;
 
+import java.util.Date;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +11,9 @@ import br.com.digivox.reactchallenge.domain.Aluguel;
 
 @Repository
 public interface AluguelRepository extends JpaRepository<Aluguel, Integer> {
+	
+	Page<Aluguel> findByDataPrevistaParaDevolucaoBetween(Date segunda, Date domingo, Pageable pageRequest);
+	
+	Page<Aluguel> findByDataDeCriacaoBetween(Date segunda, Date domingo, Pageable pageRequest);
 
 }
--- src/main/java/br/com/digivox/reactchallenge/services/AluguelService.java
+++ src/main/java/br/com/digivox/reactchallenge/services/AluguelService.java
@@ -1,5 +1,11 @@
 package br.com.digivox.reactchallenge.services;
 
+import static java.time.DayOfWeek.MONDAY;
+import static java.time.DayOfWeek.SUNDAY;
+import static java.time.temporal.TemporalAdjusters.nextOrSame;
+import static java.time.temporal.TemporalAdjusters.previousOrSame;
+
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
@@ -7,6 +13,8 @@ import java.util.Optional;
 
 import org.hibernate.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import br.com.digivox.reactchallenge.domain.Aluguel;
@@ -32,20 +40,20 @@ public class AluguelService {
 
 	public Aluguel createAluguel(Aluguel aluguel) {
 		
-		for(Livro livro : aluguel.getLivros()) {
-			livro.setStatus(LivroStatus.EM_UTILIZACAO);
-			livroService.update(livro);
-		}
-		
 		aluguel.setDataDeCriacao(new Date());
 		
-		if(aluguel.getDataDeRetirada() == null) {
+		if(aluguel.getDataDeRetirada() != null) {
 			
 			LocalDateTime dataDaRetirada = aluguel.getDataDeRetirada().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime();  
 			
 			LocalDateTime dataPrevistaParaDevolucao = dataDaRetirada.plusDays(7);
 					
 			aluguel.setDataPrevistaParaDevolucao(Date.from(dataPrevistaParaDevolucao.atZone(ZoneId.systemDefault()).toInstant()));
+			
+			for(Livro livro : aluguel.getLivros()) {
+				livro.setStatus(LivroStatus.RESERVADO);
+				livroService.update(livro);
+			}
 			
 			aluguel.setStatus(AluguelStatus.RESERVADO);
 			
@@ -54,7 +62,12 @@ public class AluguelService {
 			
 			LocalDateTime dataParaDevolucao = hoje.plusDays(7);
 					
-			aluguel.setDataDeDevolucao(Date.from(dataParaDevolucao.atZone(ZoneId.systemDefault()).toInstant()));
+			aluguel.setDataPrevistaParaDevolucao(Date.from(dataParaDevolucao.atZone(ZoneId.systemDefault()).toInstant()));
+			
+			for(Livro livro : aluguel.getLivros()) {
+				livro.setStatus(LivroStatus.EM_UTILIZACAO);
+				livroService.update(livro);
+			}
 			
 			aluguel.setStatus(AluguelStatus.EM_ANDAMENTO);
 			
@@ -70,6 +83,45 @@ public class AluguelService {
 	
 	public Aluguel update(Aluguel aluguel) {
 		return aluguelRepository.saveAndFlush(aluguel);
+	}
+	
+	public Page<Aluguel> alugueisPrevistoParaDevolucao(int page, int size) {
+		PageRequest pageRequest = PageRequest.of(page, size);
+		
+		LocalDate hoje = LocalDate.now();
+		
+		LocalDate domingoInLocalDate = hoje.with(nextOrSame(SUNDAY));
+	    LocalDate segundaInLocalDate = hoje.with(previousOrSame(MONDAY));
+	    
+	    Date domingoInDate = Date.from(domingoInLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	    Date segundaInDate = Date.from(segundaInLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	    
+		return aluguelRepository.findByDataPrevistaParaDevolucaoBetween(segundaInDate, domingoInDate, pageRequest);
+	}
+	
+	public Aluguel devolverLivroAlugado(Aluguel aluguel) {
+		for(Livro livro : aluguel.getLivros()) {
+			livroService.atualizaStatus(livro, LivroStatus.DISPONIVEL);
+		}
+		
+		aluguel.setDataDeDevolucao(new Date());
+		aluguel.setStatus(AluguelStatus.FINALIZADA);
+		
+		return update(aluguel);
+	}
+
+	public Page<Aluguel> alugadosNaSemana(Integer page, Integer size) {
+		PageRequest pageRequest = PageRequest.of(page, size);
+		
+		LocalDate hoje = LocalDate.now();
+		
+		LocalDate domingoInLocalDate = hoje.with(nextOrSame(SUNDAY));
+	    LocalDate segundaInLocalDate = hoje.with(previousOrSame(MONDAY));
+	    
+	    Date domingoInDate = Date.from(domingoInLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	    Date segundaInDate = Date.from(segundaInLocalDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+	    
+		return aluguelRepository.findByDataDeCriacaoBetween(segundaInDate, domingoInDate, pageRequest);
 	}
 
 }
